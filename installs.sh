b6adbb0eb10c682380fc4e77cd18bff6c2e31eaa
hzeyoo <hzeyoo@gmail.com>
2018-06-13 23:03:08 +0800

    Update installs.sh

--- installs.sh
+++ installs.sh
@@ -23,19 +23,6 @@ displayErr() {
     exit 1;
 }
 
-    output " "
-    output "Make sure you double check before hitting enter! Only one shot at these!"
-    output " "
-    read -e -p "Enter time zone (e.g. America/New_York) : " TIME
-    read -e -p "Server name (no http:// or www. just example.com) : " server_name
-    read -e -p "Are you using a subdomain (pool.example.com?) [y/N] : " sub_domain
-    read -e -p "Enter support email (e.g. admin@example.com) : " EMAIL
-    read -e -p "Set stratum to AutoExchange? i.e. mine any coinf with BTC address? [y/N] : " BTC
-    read -e -p "Please enter a new location for /site/adminRights this is to customize the admin entrance url (e.g. myAdminpanel) : " admin_panel
-    read -e -p "Enter your Public IP for admin access (http://www.whatsmyip.org/) : " Public
-    read -e -p "Install Fail2ban? [Y/n] : " install_fail2ban
-    read -e -p "Install UFW and configure ports? [Y/n] : " UFW
-    read -e -p "Install LetsEncrypt SSL? IMPORTANT! You MUST have your domain name pointed to this server prior to running the script!! [Y/n]: " ssl_install
     
     output " "
     output "Updating system and installing required packages."
